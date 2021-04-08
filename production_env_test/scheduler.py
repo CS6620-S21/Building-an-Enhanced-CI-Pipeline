7622de904c99d9c69a5b47b96c127c7fb269aa96
yaqinz <zyqwenny@gmail.com>
2021-04-08 13:39:56 -0400

    reset ci and production env

--- production_env_test/scheduler.py
+++ production_env_test/scheduler.py
@@ -4,6 +4,6 @@ import subprocess
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
 
-    scheduler.add_job(subprocess.run, 'interval', minutes=10, args=(["locust" ,"-f","locustfile.py","--host", "http://34.102.157.148", "--users", "300", "--spawn-rate", "2", "--run-time", "30s", "--headless"], ))
+    scheduler.add_job(subprocess.run, 'interval', hours=1, args=(["locust" ,"-f","locustfile.py","--host", "http://34.102.157.148", "--users", "300", "--spawn-rate", "2", "--run-time", "30s", "--headless"], ))
 
     scheduler.start()
