3f67e6f92a3de13a610568bd47fdb844a91074ed
yaqinz <zyqwenny@gmail.com>
2021-04-08 13:58:06 -0400

    fix ci and change style again

--- production_env_test/scheduler.py
+++ production_env_test/scheduler.py
@@ -4,13 +4,13 @@ import subprocess
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
 
-    scheduler.add_job(subprocess.run, 
-                      'interval', 
-                      hours=1, 
+    scheduler.add_job(subprocess.run,
+                      'interval',
+                      hours=1,
                       args=([
-                          "locust", "-f", "locustfile.py", "--host", 
-                          "http://34.102.157.148", "--users", "300", 
-                          "--spawn-rate", "2", "--run-time", "30s", 
+                          "locust", "-f", "locustfile.py", "--host",
+                          "http://34.102.157.148", "--users", "300",
+                          "--spawn-rate", "2", "--run-time", "30s",
                           "--headless"
                       ], ))
 
